cec63b7a1ca6d2e26f9af7f7a8b1a4b99df07e01
Steve Ivy <steveivy@gmail.com>
2020-02-28 15:38:23 -0700

    add apache beam implementation

--- beam/README.md
+++ beam/README.md
@@ -2,3 +2,22 @@
 
 Experimental implementation of the Kuiil test pipeline with the Apache Beam Python SDK.
 
+## Getting Source Data
+
+### Character Data
+
+```
+pipenv run python beam/kuill.py get_characters --output characters.csv
+```
+
+### Species Data
+
+```
+pipenv run python beam/kuill.py get_species --output species.csv
+```
+
+## Run the Pipeline
+
+```
+pipenv run python beam/kuill.py pipeline --characters characters.csv --species species.csv --output out.csv
+```
--- beam/kuill.py
+++ beam/kuill.py
@@ -14,8 +14,8 @@ logging.getLogger("apache_beam").setLevel(logging.ERROR)
 
 class ParseCharacterFn(beam.DoFn):
     """
-    Parses raw character data into a tuple of
-    species_i and python dictionary of the data
+    Parses raw character data into a
+    dictionary of the data
 
     appearances,name,height,species_id
     """
@@ -29,18 +29,22 @@ class ParseCharacterFn(beam.DoFn):
     def process(self, elem):
         """
         Returns a key, val tuple of (species_id, {character data})
-        so that the data can b compbined with the species data
+        so that the data can be combined with the species data
         by species_id
         """
         try:
             row = list(csv.reader([elem]))[0]
-            logging.debug("Got row: %s", row)
+            logging.debug("Got character row: %s", row)
+            appearances, name, height, species_id = row
+            appearances = int(appearances)
+            height = int(height) if height != "unknown" else -1.0
+            species_id = int(float(species_id)) if species_id else 0
 
             yield {
-                "appearances": row[0],
-                "name": row[1],
-                "height": float(row[2]) if row[2] != "unknown" else -1.0,
-                "species_id": row[3],
+                "appearances": appearances,
+                "name": name,
+                "height": height,
+                "species_id": species_id,
             }
         except Exception as e:
             self.num_parse_errors.inc()
@@ -66,9 +70,9 @@ class ParseSpeciesFn(beam.DoFn):
         """
         try:
             row = list(csv.reader([elem]))[0]
-            logging.debug("Got row: %s", row)
+            logging.debug("Got species row: %s", row)
             yield {
-                "species_id": row[0],
+                "species_id": int(row[0]) if row[0] else 0,
                 "species": row[1],
             }
         except Exception as e:
@@ -102,11 +106,14 @@ def get_species_command(args):
 
 
 def run(argv=None, save_main_session=True):
-    """Main entry point; defines and runs the kuill pipeline."""
+    """
+    Main entry point; defines and runs the kuill pipeline.
+    """
     parser = argparse.ArgumentParser()
 
     sp = parser.add_subparsers()
-    get_characters_parser = sp.add_parser("get_data")
+    #
+    get_characters_parser = sp.add_parser("get_characters")
     get_characters_parser.add_argument("--output", default="characters.csv")
     get_characters_parser.set_defaults(command=get_characters_command)
 
@@ -117,9 +124,14 @@ def run(argv=None, save_main_session=True):
     pipeline_parser = sp.add_parser("pipeline")
 
     pipeline_parser.add_argument(
-        '--input', type=str,
+        '--characters', type=str,
         required=True,
-        help='Path to the input file.')
+        help='Path to an input file.')
+
+    pipeline_parser.add_argument(
+        '--species', type=str,
+        required=True,
+        help='Path to an input file.')
 
     pipeline_parser.add_argument(
         '--output', type=str,
@@ -138,52 +150,102 @@ def run(argv=None, save_main_session=True):
     options = PipelineOptions(pipeline_args)
     options.view_as(SetupOptions).save_main_session = save_main_session
 
-    def join_lists(k, v):
+    def join_data(k, v):
+        logging.debug("join_data: %s", (k, v))
         import itertools
-        itertools.product(v['characters'], v['species'])
+        return itertools.product(v['characters'], v['species'])
+
+    def merge_data(row):
+        logging.debug("merge_data: %s", row)
+        character_data, species_data = row
+        character_data.update(species_data)
+        del character_data['species_id']
+        return character_data
+
+    def by_all_appearances(row):
+        logging.debug("by_appearances: %s", row)
+        return (row["appearances"], row)
+
+    def appearance_key_for_element(element):
+        """
+        element is a dictionary of character data
+        return the value of the "appearances" key
+        """
+        return element["appearances"]
+
+    def height_key_for_element(element):
+        """
+        element is a dictionary of character data
+        return the value of the "appearances" key
+        """
+        return element["height"]
+
+    def format_csv(data):
+        lines = ["{height},{appearances},{name},{species}".format(
+            **row) for row in data]
+        return "\n".join(lines)
+
+    def resplit_data(data):
+        """
+        data is a list with one element, a list of rows?
+        """
+        logging.debug("resplit_data: %s", data)
+        for row in data:
+            yield row
 
     with beam.Pipeline(options=options) as p:
-        (
+        char_inputs = (
             p
-            | 'ReadInputText' >> beam.io.ReadFromText(
-                args.input, skip_header_lines=1)
+            | 'ReadCharInputText' >> beam.io.ReadFromText(
+                args.characters, skip_header_lines=1)
+        )
+
+        spec_inputs = (
+            p
+            | 'ReadSpecInputText' >> beam.io.ReadFromText(
+                args.species, skip_header_lines=1)
         )
 
         characters = (
-            p
-            | 'CharacterData' >> CharacterData()
+            char_inputs
+            | "parse chars" >> beam.ParDo(ParseCharacterFn())
             | "key_char" >> beam.Map(
                 lambda c: (c["species_id"], c))
         )
-        # logging.debug(characters)
 
         species = (
-            p
-            | 'SpeciesData' >> SpeciesData()
+            spec_inputs
+            | beam.ParDo(ParseSpeciesFn())
             | "key_spec" >> beam.Map(
                 lambda s: (s["species_id"], s))
         )
-        logging.debug(species)
 
-        joined = {"characters": characters,
-                  "species": species} | beam.CoGroupByKey()
-        # | beam.FlatMap(join_lists)
-        # | 'WriteCharacterData' >> beam.io.WriteToText(args.output)
+        joined = (
+            {"characters": characters,
+                "species": species}
+            | beam.CoGroupByKey()
+            | beam.FlatMapTuple(join_data)
+        )
 
-        logging.debug(joined)
+        merged = (
+            joined
+            | beam.Map(merge_data)
+        )
 
-        # (
-        #     p
-        #     # | 'ReadInputText' >> beam.io.ReadFromText(
-        #     #     args.input, skip_header_lines=1)
-        #     # | 'CharacterData' >> CharacterData()
-        #     # | "key_char" >> beam.Map(
-        #     #     lambda c: (c["species_id"], c))
-        #     # | 'SpeciesData' >> SpeciesData()
-        #     # | "key_spec" >> beam.Map(
-        #     #     lambda s: (s["species_id"], s))
-        #     | 'WriteCharacterData' >> beam.io.WriteToText(args.output)
-        # )
+        top = (
+            merged
+            | "top by appearances" >> beam.combiners.Top.Of(
+                10, key=appearance_key_for_element)
+            | "re-split for height" >> beam.FlatMap(resplit_data)
+            | "top by height" >> beam.combiners.Top.Of(
+                10, key=height_key_for_element)
+        )
+
+        output = (  # noqa
+            top
+            | "format_csv" >> beam.Map(format_csv)
+            | 'WriteCharacterData' >> beam.io.WriteToText(args.output)
+        )
 
 
 if __name__ == "__main__":
